f37132025e4a18b73b7de241eb14e7013ddee3c3
Jacob <jaagup.tomingas@gmail.com>
2023-11-30 04:25:51 +0200

    master: build fix

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -4,21 +4,21 @@ const Page = () => {
     return (
         <main>
             <article className="text-justify bg-bg1 px-[15%] pt-32 pb-16 space-y-4 ">
-                <p>Hello!</p>
+                <p>{"Hello!"}</p>
 
-                <p>I was a student and also the student council president in kood/Jõhvi school. This position gave me the opportunity to organize school life, so that the students felt accommodated. Many of my school tasks required me to take the team lead role and organize the projects with my team. For that, we usually had stand-ups every weekday in the morning.</p>
+                <p>{"I was a student and also the student council president in kood/Jõhvi school. This position gave me the opportunity to organize school life, so that the students felt accommodated. Many of my school tasks required me to take the team lead role and organize the projects with my team. For that, we usually had stand-ups every weekday in the morning."}</p>
 
-                <p>The biggest and most exciting project in kood/Jõhvi, was to build an online multiplayer first-person shooter, only using Rust. We had an 8 person team, the frontend team used raycasting to build a feel for 3D inside the game and the backend took care of the Websockets and the ECS architecture. After the project was finished, we let other people at our school try it out and play with each other from their own computers. Aside from that, we also built many projects using REST API using Go and ReactJS.</p>
+                <p>{"The biggest and most exciting project in kood/Jõhvi, was to build an online multiplayer first-person shooter, only using Rust. We had an 8 person team, the frontend team used raycasting to build a feel for 3D inside the game and the backend took care of the Websockets and the ECS architecture. After the project was finished, we let other people at our school try it out and play with each other from their own computers. Aside from that, we also built many projects using REST API using Go and ReactJS."}</p>
                 
-                <p>Until recently, I was pushing forward with MindSee, a top 20 student-led startup in Estonia's 'Ajujaht' series. We continuously improved ourselves by adding new user features. I'm open to many different opportunities for change, which led me to this startup and has given me valuable connections and experiences.</p>
+                <p>{"Until recently, I was pushing forward with MindSee, a top 20 student-led startup in Estonia's 'Ajujaht' series. We continuously improved ourselves by adding new user features. I'm open to many different opportunities for change, which led me to this startup and has given me valuable connections and experiences."}</p>
 
-                <p>I have also participated in different hobbies, such as fencing, chess, robotics, guitar, piano, choir singing, karate, thai-boxing and more. Currently I am still learning the guitar and join boxing sessions from time to time</p>
+                <p>{"I have also participated in different hobbies, such as fencing, chess, robotics, guitar, piano, choir singing, karate, thai-boxing and more. Currently I am still learning the guitar and join boxing sessions from time to time"}</p>
                 
-                <p>Thank you for considering my application. I'm excited about the possibility of joining Pipedrive and contributing to its success. I look forward to the opportunity to discuss my qualifications further.</p>
+                <p>{"Thank you for considering my application. I'm excited about the possibility of joining Pipedrive and contributing to its success. I look forward to the opportunity to discuss my qualifications further."}</p>
                 
-                <p>Thank you for your time, and I hope to hear from you soon!</p>
+                <p>{"Thank you for your time, and I hope to hear from you soon!"}</p>
                 
-                <p>Jaagup Tomingas</p>
+                <p>{"Jaagup Tomingas"}</p>
             </article>
 
         </main>
