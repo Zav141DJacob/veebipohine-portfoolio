18fdbc92191a2b8bf3516f4c4dd58b177924a626
Jacob <jaagup.tomingas@gmail.com>
2023-12-13 10:22:34 +0200

    master: testing font fix for mobile; TailwindCSS SSR

--- src/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-const Page = () => {
-    return (
-        <main className="">
-            This is About me paege
-        </main>
-    )
-}
-
-export default Page;
--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,15 +1,21 @@
 import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
+import Head from 'next/head'
 
 const Template: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
-        <div className="min-h-screen flex flex-col transition-all">
-            {/* <ViewTransition/> */}
-            <Header />
-            <div className="grow flex flex-col">
-                {children}
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width" />
+            </Head>
+            <div className="min-h-screen flex flex-col transition-all">
+                {/* <ViewTransition/> */}
+                <Header />
+                <div className="grow flex flex-col">
+                    {children}
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
